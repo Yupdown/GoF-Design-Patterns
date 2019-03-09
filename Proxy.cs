2c406577e5ae5e5c56808a6fed84c44bf1b40b29
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-03-10 00:38:17 +0900

    [Add] Proxy Pattern

---
+++ Proxy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Proxy
+{
+    public class Subject
+    {
+        public void Operate()
+        {
+            Console.WriteLine("Operate Subject");
+        }
+    }
+
+    public class ProxySubject
+    {
+        private Subject _subject;
+
+        public ProxySubject(Subject subject)
+        {
+            _subject = subject;
+        }
+
+        public void Operate()
+        {
+            _subject.Operate();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        ProxySubject proxy = new ProxySubject(new Subject());
+
+        proxy.Operate();
+    }
+}
