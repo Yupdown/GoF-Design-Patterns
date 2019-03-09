77d189b3f77c8c204d48b3875d28e7f1af589ba8
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-03-09 22:51:03 +0900

    [New] GoFDesignPattern

---
+++ Adapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Adapter
+{
+    public interface IAdapter
+    {
+        void OperateAdapter();
+    }
+
+    public class Adaptee
+    {
+        public void OperateAdaptee()
+        {
+            Console.WriteLine("Operate Adaptee");
+        }
+    }
+
+    public class ReferenceAdapter : IAdapter
+    {
+        private Adaptee _adaptee;
+
+        public ReferenceAdapter(Adaptee adaptee)
+        {
+            _adaptee = adaptee;
+        }
+
+        public void OperateAdapter()
+        {
+            _adaptee.OperateAdaptee();
+        }
+    }
+
+    public class ImplementationAdapter : Adaptee, IAdapter
+    {
+        public void OperateAdapter()
+        {
+            OperateAdaptee();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        IAdapter adapter;
+
+        adapter = new ReferenceAdapter(new Adaptee());
+        adapter.OperateAdapter();
+
+        adapter = new ImplementationAdapter();
+        adapter.OperateAdapter();
+    }
+}
