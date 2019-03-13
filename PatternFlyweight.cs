e63163be3026c4be60aaa3060b486f915f5ca5de
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-03-13 19:40:10 +0900

    [Add] Flyweight Pattern

---
+++ PatternFlyweight.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PatternFlyweight
+{
+    public interface IFlyweight
+    {
+        void Operation();
+    }
+
+    public class Flyweight : IFlyweight
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Operating Flyweight (" + GetHashCode() + ")");
+        }
+    }
+
+    public class FlyweightFactory
+    {
+        private Dictionary<string, IFlyweight> _flyweights;
+
+        public FlyweightFactory()
+        {
+            _flyweights = new Dictionary<string, IFlyweight>();
+        }
+
+        public IFlyweight GetFlyweight(string key)
+        {
+            IFlyweight flyweight;
+
+            _flyweights.TryGetValue(key, out flyweight);
+
+            if (flyweight == null)
+            {
+                flyweight = new Flyweight();
+
+                _flyweights.Add(key, flyweight);
+            }
+
+            return flyweight;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        FlyweightFactory flyweightFactory = new FlyweightFactory();
+
+        IFlyweight[] flyweights = new IFlyweight[]
+        {
+            flyweightFactory.GetFlyweight("A"),
+            flyweightFactory.GetFlyweight("A"),
+            flyweightFactory.GetFlyweight("B"),
+            flyweightFactory.GetFlyweight("C"),
+            flyweightFactory.GetFlyweight("C")
+        };
+
+        foreach (IFlyweight flyweight in flyweights)
+        {
+            flyweight.Operation();
+        }
+    }
+}
