75666b6b7edc4abd63e1e384ddcb30c9486cc040
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-03-13 11:43:08 +0900

    [Add] Decorator Pattern, Facade Pattern

---
+++ PatternDecorator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class PatternDecorator
+{
+    public interface IComponent
+    {
+        void Operate();
+    }
+
+    public class Component : IComponent
+    {
+        public void Operate()
+        {
+            Console.WriteLine("Operating Component");
+        }
+    }
+
+    public abstract class Decorator : IComponent
+    {
+        protected IComponent _component;
+
+        public Decorator(IComponent component)
+        {
+            _component = component;
+        }
+
+        public abstract void Operate();
+    }
+
+    public class DecoratorA : Decorator
+    {
+        public DecoratorA(IComponent component) : base(component)
+        {
+
+        }
+
+        public override void Operate()
+        {
+            _component.Operate();
+
+            Console.WriteLine("Powered by DecoratorA");
+        }
+    }
+
+    public class DecoratorB : Decorator
+    {
+        public DecoratorB(IComponent component) : base(component)
+        {
+
+        }
+
+        public override void Operate()
+        {
+            _component.Operate();
+
+            Console.WriteLine("Powered by DecoratorB");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        IComponent decoratorA = new DecoratorA(new Component());
+        IComponent decoratorB = new DecoratorB(decoratorA);
+
+        decoratorA.Operate();
+        Console.WriteLine();
+
+        decoratorB.Operate();
+    }
+}
---
+++ PatternFacade.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+public class PatternFacade
+{
+    public class ClassA
+    {
+        public void Operate()
+        {
+            Console.WriteLine("Operating ClassA");
+        }
+    }
+
+    public class ClassB
+    {
+        public void Operate()
+        {
+            Console.WriteLine("Operating ClassB");
+        }
+    }
+
+    public class ClassC
+    {
+        public void Operate()
+        {
+            Console.WriteLine("Operating ClassC");
+        }
+    }
+
+    public class Facade
+    {
+        private ClassA _classA;
+        private ClassB _classB;
+        private ClassC _classC;
+
+        public Facade(ClassA classA, ClassB classB, ClassC classC)
+        {
+            _classA = classA;
+            _classB = classB;
+            _classC = classC;
+        }
+
+        public void OperateClassA()
+        {
+            _classA.Operate();
+        }
+
+        public void OperateClassB()
+        {
+            _classB.Operate();
+        }
+
+        public void OperateClassC()
+        {
+            _classC.Operate();
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        ClassA classA = new ClassA();
+        ClassB classB = new ClassB();
+        ClassC classC = new ClassC();
+
+        Facade facade = new Facade(classA, classB, classC);
+
+        facade.OperateClassA();
+        facade.OperateClassB();
+        facade.OperateClassC();
+    }
+}
