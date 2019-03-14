f8f90b226fce69408fce1a4ac882ce94a598cbf6
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-03-14 16:04:43 +0900

    [Add] Abstract Factory Pattern

---
+++ Patterns/PatternAbstractFactory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class PatternAbstractFactory
+{
+    public interface IAbstractFactory
+    {
+        IProduct CreateProduct();
+    }
+
+    public class AbstractFactoryA : IAbstractFactory
+    {
+        public IProduct CreateProduct()
+        {
+            return new ProductA();
+        }
+    }
+    
+    public class AbstractFactoryB : IAbstractFactory
+    {
+        public IProduct CreateProduct()
+        {
+            return new ProductB();
+        }
+    }
+
+    public interface IProduct
+    {
+        void Operation();
+    }
+
+    public class ProductA : IProduct
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Operating ProductA");
+        }
+    }
+
+    public class ProductB : IProduct
+    {
+        public void Operation()
+        {
+            Console.WriteLine("Operating ProductB");
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        IAbstractFactory abstractFactory;
+        IProduct product;
+
+        abstractFactory = new AbstractFactoryA();
+        product = abstractFactory.CreateProduct();
+
+        product.Operation();
+
+        abstractFactory = new AbstractFactoryB();
+        product = abstractFactory.CreateProduct();
+
+        product.Operation();
+    }
+}
