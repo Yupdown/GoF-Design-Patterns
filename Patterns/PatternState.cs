c87763a4e8169f8cc1484c00d21f419c78facfa9
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-04-02 00:50:57 +0900

    [Add] State Pattern

--- Patterns/PatternState.cs
+++ Patterns/PatternState.cs
@@ -2,8 +2,59 @@
 
 public class PatternState
 {
+    public class Context
+    {
+        private IState _state;
+
+        public Context()
+        {
+            _state = new StateA();
+        }
+
+        public void SetState(IState state)
+        {
+            _state = state;
+        }
+
+        public void Operation()
+        {
+            _state.Operation(this);
+        }
+    }
+
+    public interface IState
+    {
+        void Operation(Context context);
+    }
+
+    public class StateA : IState
+    {
+        public void Operation(Context context)
+        {
+            Console.WriteLine("Operate StateA");
+
+            context.SetState(new StateB());
+        }
+    }
+
+    public class StateB : IState
+    {
+        public void Operation(Context context)
+        {
+            Console.WriteLine("Operate StateB");
+
+            context.SetState(new StateA());
+        }
+    }
+
+
     public static void Main(string[] args)
     {
-
+        Context context = new Context();
+        
+        for (int num = 0; num < 3; num++)
+        {
+            context.Operation();
+        }
     }
 }
