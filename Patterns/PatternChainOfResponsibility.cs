f0e02b3feeb64404b7b120b6809a84aa1d51888c
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-04-01 17:22:41 +0900

    [Add] Chain of Responsibility Pattern

--- Patterns/PatternChainOfResponsibility.cs
+++ Patterns/PatternChainOfResponsibility.cs
@@ -2,8 +2,73 @@
 
 public class PatternChainOfResponsibility
 {
+    public abstract class Handler
+    {
+        private Handler _next;
+
+        public void SetNextHandler(Handler handler)
+        {
+            _next = handler;
+        }
+
+        public void HandleRequest<Receiver>() where Receiver : Handler
+        {
+            if (this is Receiver)
+            {
+                Operate();
+            }
+
+            if (_next != null)
+            {
+                _next.HandleRequest<Receiver>();
+            }
+        }
+
+        public abstract void Operate();
+    }
+
+    public class ReceiverA : Handler
+    {
+        public override void Operate()
+        {
+            Console.WriteLine("Operating ReceiverA");
+        }
+    }
+
+    public class ReceiverB : Handler
+    {
+        public override void Operate()
+        {
+            Console.WriteLine("Operating ReceiverB");
+        }
+    }
+
+    public class ReceiverC : Handler
+    {
+        public override void Operate()
+        {
+            Console.WriteLine("Operating ReceiverC");
+        }
+    }
+
     public static void Main(string[] args)
     {
+        Handler[] receivers = new Handler[]
+        {
+            new ReceiverA(),
+            new ReceiverB(),
+            new ReceiverC()
+        };
 
+        for (int index = 0; index < receivers.Length - 1; index++)
+        {
+            receivers[index].SetNextHandler(receivers[index + 1]);
+        }
+
+        Handler handler = receivers[0];
+
+        handler.HandleRequest<ReceiverA>();
+        handler.HandleRequest<ReceiverB>();
+        handler.HandleRequest<ReceiverC>();
     }
 }
