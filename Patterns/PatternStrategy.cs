e6c881dbe6e5f11129dc52f6d1ba89e056ad50dd
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-04-02 02:24:11 +0900

    [Add] Strategy Pattern

--- Patterns/PatternStrategy.cs
+++ Patterns/PatternStrategy.cs
@@ -2,8 +2,50 @@
 
 public class PatternStrategy
 {
+    public class Context
+    {
+        private IStrategy _strategy;
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            _strategy = strategy;
+        }
+
+        public void Operation()
+        {
+            Console.WriteLine(_strategy.Algorithm("Context"));
+        }
+    }
+
+    public interface IStrategy
+    {
+        string Algorithm(string context);
+    }
+
+    public class StrategyA : IStrategy
+    {
+        public string Algorithm(string context)
+        {
+            return "Operate " + context + " as StrategyA";
+        }
+    }
+
+    public class StrategyB : IStrategy
+    {
+        public string Algorithm(string context)
+        {
+            return "Operate " + context + " as StrategyB";
+        }
+    }
+
     public static void Main(string[] args)
     {
+        Context context = new Context();
 
+        context.SetStrategy(new StrategyA());
+        context.Operation();
+
+        context.SetStrategy(new StrategyB());
+        context.Operation();
     }
 }
