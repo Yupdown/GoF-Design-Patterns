1c79b3f8adad937c6fc5a9c20450f2036db657c6
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-04-02 03:52:29 +0900

    [Add] Visitor Pattern

--- Patterns/PatternVisitor.cs
+++ Patterns/PatternVisitor.cs
@@ -2,8 +2,66 @@
 
 public class PatternVisitor
 {
+    public interface IElement
+    {
+        void Accept(IVisitor visitor);
+    }
+
+    public class ElementA : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitElementA(this);
+        }
+
+        public void OperationA()
+        {
+            Console.WriteLine("Operate A");
+        }
+    }
+
+    public class ElementB : IElement
+    {
+        public void Accept(IVisitor visitor)
+        {
+            visitor.VisitElementB(this);
+        }
+
+        public void OperationB()
+        {
+            Console.WriteLine("Operate B");
+        }
+    }
+
+    public interface IVisitor
+    {
+        void VisitElementA(ElementA element);
+        void VisitElementB(ElementB element);
+    }
+
+    public class VisitorA : IVisitor
+    {
+        public void VisitElementA(ElementA element)
+        {
+            element.OperationA();
+        }
+
+        public void VisitElementB(ElementB element)
+        {
+            element.OperationB();
+        }
+    }
+
     public static void Main(string[] args)
     {
+        IVisitor visitor = new VisitorA();
 
+        IElement element;
+
+        element = new ElementA();
+        element.Accept(visitor);
+
+        element = new ElementB();
+        element.Accept(visitor);
     }
 }
