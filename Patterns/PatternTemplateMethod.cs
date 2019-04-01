80f4aa493ac1812eb327116a5cfbe145e661b367
YUPLAPTOP-ASUSG\Yup <wts1597@gmail.com>
2019-04-02 03:09:44 +0900

    [Add] Template Method Pattern

--- Patterns/PatternTemplateMethod.cs
+++ Patterns/PatternTemplateMethod.cs
@@ -2,8 +2,35 @@
 
 public class PatternTemplateMethod
 {
+    public abstract class AbstractClass
+    {
+        public void TemplateMethod()
+        {
+            PrimitiveA();
+            PrimitiveB();
+        }
+
+        protected abstract void PrimitiveA();
+        protected abstract void PrimitiveB();
+    }
+
+    public class SubClass : AbstractClass
+    {
+        protected override void PrimitiveA()
+        {
+            Console.WriteLine("Operate PrimitiveA in SubClass");
+        }
+
+        protected override void PrimitiveB()
+        {
+            Console.WriteLine("Operate PrimitiveB in SubClass");
+        }
+    }
+
     public static void Main(string[] args)
     {
+        AbstractClass abstractClass = new SubClass();
 
+        abstractClass.TemplateMethod();
     }
 }
